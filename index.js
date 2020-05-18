e9d96e58ba6bbd5b6ef0469650670391b782d408
Justin Ochoa <59962484+justinochoa5792@users.noreply.github.com>
2020-05-17 22:10:23 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-//codewars kata
-function isPangram(str){
-    var alphabet = ['a', 'b', 'c', 'd', 'e',
-           'f', 'g', 'h', 'i', 'j',
-           'l', 'm', 'n', 'o', 'p',
-           'q', 'r', 's', 't', 'u',
-           'v', 'w', 'x', 'y', 'z'
-         ];
-         str=str.toLowerCase();
-         for (var i = 0; i < alphabet.length; i++) {
-             if (str.indexOf(alphabet[i])<0) {
-               return false;
-             }
-         }
-         return true
-   }
+function check(a, x) {
+    var value = a.indexOf(x);
+    if (value > -1) {
+    return(true);
+    } else {
+    return(false);
+    }
+  }
